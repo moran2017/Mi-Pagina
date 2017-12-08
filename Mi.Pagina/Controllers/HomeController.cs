3dbb268cc182b2437789e349e5bb69e94dcad852
moran2017 <31378556+moran2017@users.noreply.github.com>
2017-12-08 13:24:09 -0600

    Initial commit

---
+++ Mi.Pagina/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Net.Mail;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Mi.Pagina.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("Body");
+        }
+
+        public ActionResult Body()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }       
+     
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Mi.Pagina/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mi.Pagina.MvcApplication" Language="C#" %>
